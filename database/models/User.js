a499264bdb69a8fd5add10f5cb7fa09db99684d2
Joseph Nahm <josephnahm1@gmail.com>
2022-04-02 13:13:16 -0700

    fixed bug where prevent multiple users inserted

--- database/models/User.js
+++ database/models/User.js
@@ -4,8 +4,8 @@ const userSchema = new mongoose.Schema({
   username: { type: String, unique: true, required: true, index: true },
   password: String,
   profPhoto: String,
-  following: [{ type: String, unique: true}],
-  followers: [{ type: String, unique: true}],
+  following: [],
+  followers: [],
 });
 
 module.exports = mongoose.model("User", userSchema);
