b1f1201fd064197f75c4f3b9beca715f0e280fd4
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-04-04 20:30:30 -0700

    remove password from user model

--- database/models/User.js
+++ database/models/User.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   username: { type: String, unique: true, required: true, index: true },
-  password: String,
   profPhoto: String,
   following: [],
   followers: [],
