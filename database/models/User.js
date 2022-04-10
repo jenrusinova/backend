ff14075a7b006f20378f7be9bf44806fa2a38cb6
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-04-09 17:44:32 -0700

    add email

--- database/models/User.js
+++ database/models/User.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   username: { type: String, unique: true, required: true, index: true },
   password: {type: String, required: true},
+  email: {type: String, required: true},
   profPhoto: String,
   following: [],
   followers: [],
