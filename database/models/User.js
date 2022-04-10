24623b8aad58000e19fa3ca1ee049e24553c51b1
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-04-09 17:43:26 -0700

    add password

--- database/models/User.js
+++ database/models/User.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   username: { type: String, unique: true, required: true, index: true },
+  password: {type: String, required: true},
   profPhoto: String,
   following: [],
   followers: [],
