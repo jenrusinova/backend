a868e1db3b752e3c551eca155818d9848e46d92c
Jenya Rusinova <eugeniya.rusinova@gmail.com>
2022-04-11 12:52:42 -0700

    Added session middleware

--- server/index.js
+++ server/index.js
@@ -3,20 +3,30 @@ const app = express();
 const { serverPort } = require("../constants");
 const mongoose = require ("mongoose");
 const session = require("express-session");
+const passport = require ('passport');
+// const routes = require ('./routes');
+const bcrypt = require("bcryptjs");
+
+require ('dotenv').config();
+require ('../config/passport');
+app.use (passport.initialize());
 
 const MongoStore = require ('connect-mongo')(session);
 
-const db = require("../database");
+const db = require("../database");//connection
 
 const dbString = 'mongodb://localhost:27017/sessions';
 const dbOptions = {
   useNewUrlParser: true,
   useUnifiedTopology:true
 }
-
+//
 const connection = mongoose.createConnection(dbString, dbOptions);
 
-app.use(express.json());
+// app.use(express.json());
+// app.use(express.urlencoded({extended: true}));
+// app.use(bodyParser.json());
+
 
 const userRouter = require("./routes/User");
 const postRouter = require("./routes/Post");
@@ -26,11 +36,10 @@ const sessionStore = new MongoStore({
   collection: 'sessions'
 });
 
-app.use("/user", userRouter);
-app.use("/post", postRouter);
 
+console.log(process.env.SECRET);
 app.use(session({
-  secret: 'some secret',
+  secret: process.env.SECRET,
   resave: false,
   saveUninitialized: true,
   store: sessionStore,
@@ -38,6 +47,17 @@ app.use(session({
     maxAge: 1000 * 60 * 60 * 24 //equals 1 day
   }
 }))
+
+//passport middleware
+
+require ('../config/passport');
+app.use (passport.initialize());
+app.use(passport.session());
+
+app.use("/user", userRouter);
+app.use("/post", postRouter);
+
+
 
 app.get('/', (req, res, next) => {
   res.send('sessions');
