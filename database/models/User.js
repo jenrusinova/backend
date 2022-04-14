ad5623e66cd09d2ef03d738101fac1f82486347d
Jenya Rusinova <eugeniya.rusinova@gmail.com>
2022-04-13 20:47:20 -0700

    Deleted required property from the password

--- database/models/User.js
+++ database/models/User.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   username: { type: String, unique: true, required: true, index: true },
   email: {type: String, required: true},
-  password: {type: String, required: true},
+  password: {type: String},
   profPhoto: String,
   following: [{ type: mongoose.Schema.ObjectId, ref: "User" }],
   followers: [],
