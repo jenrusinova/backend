592637ab0dcc109d27c51fe3e18b2a5777d2ce68
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-04-15 16:36:42 -0700

    complete twitter strategy

--- config/passport.js
+++ config/passport.js
@@ -3,46 +3,47 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 var GoogleStrategy = require('passport-google-oidc');
 const TwitterStrategy = require('passport-twitter').Strategy;
-const connection = require ('../database/index.js');
-const User = require('../database/models/User.js');
-const Credentials = require('../database/models/FederatedCredentials.js');
+
 // const validPassword = require ('../lib/passwordUtils.js').validPassword;
 const bcrypt = require('bcrypt');
 
+const connection = require('../database/index.js');
+const User = require('../database/models/User.js');
+const Credentials = require('../database/models/FederatedCredentials.js');
 
 passport.use(new LocalStrategy(function verify(username, password, cb) {
   console.log('username', username);
   console.log('input password', password);
-  User.findOne({username: username})
+  User.findOne({ username: username })
     .then((user) => {
-      if (!user){
+      if (!user) {
         console.log('no such user');
-        return cb (null, false);
+        return cb(null, false);
       }
 
-  console.log('received password', password);
-  console.log('password from db', user.password);
-  bcrypt.compare(password, user.password, (err, res) => {
-    if (err) {
-      console.error(err);
-      console.log('password is not valid');
-      return cb (null, false);
-    }
-    else {
-      if (res){
-      console.log('password is valid');
-      return cb(null, user);
-      } else {
-        console.log('password is not valid');
-        return cb (null, false);
-      }
-    }
-  })
-       })
-        .catch ((err) => {
-          cb (err);
-        });
-  }));
+      console.log('received password', password);
+      console.log('password from db', user.password);
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (err) {
+          console.error(err);
+          console.log('password is not valid');
+          return cb(null, false);
+        }
+        else {
+          if (res) {
+            console.log('password is valid');
+            return cb(null, user);
+          } else {
+            console.log('password is not valid');
+            return cb(null, false);
+          }
+        }
+      })
+    })
+    .catch((err) => {
+      cb(err);
+    });
+}));
 
 
 passport.use(new GoogleStrategy({
@@ -97,74 +98,72 @@ passport.use(new GoogleStrategy({
   }
 ));
 
+//     select all data from federated credentials where provider = issuer and subject = profileid
+// db.get('SELECT * FROM federated_credentials WHERE provider = ? AND subject = ?', [
+//   issuer,
+//   profile.id
+// ], function (err, row) {
+//   if (err) { return cb(err); }
+//   //if no data is found
+//   if (!row) {
+//     //add this user to User table
+//     db.run('INSERT INTO users (name) VALUES (?)', [
+//       profile.displayName
+//     ], function (err) {
+//       if (err) { return cb(err); }
+
+//       var id = this.lastID;
+//       //insert new credentials id, issuer and profile.id
+
+
+
+//       db.run('INSERT INTO federated_credentials (user_id, provider, subject) VALUES (?, ?, ?)', [
+//         id,
+//         issuer,
+//         profile.id
+//       ], function (err) {
+//         if (err) { return cb(err); }
+//         var user = {
+//           id: id,
+//           name: profile.displayName
+//         };
+//         return cb(null, user);
+//       });
+//     });
+//     //if the credentials already exists
+//   } else {
+//     db.get('SELECT rowid AS id, * FROM users WHERE rowid = ?', [row.user_id], function (err, row) {
+//       if (err) { return cb(err); }
+//       if (!row) { return cb(null, false); }
+//       return cb(null, row);
+//     });
+//   }
+// });
+
 passport.use(new TwitterStrategy({
   consumerKey: process.env.TWITTER_CONSUMER_KEY,
   consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
   callbackURL: 'http://localhost:3000/user/auth/twitter/callback',
   userProfileURL: 'https://api.twitter.com/1.1/account/verify_credentials.json?include_email=true',
-  },
+},
   function (token, tokenSecret, profile, cb) {
-    console.log('TEST ', profile)
-    User.findOrCreate({ twitterId: profile.id }, function (err, user) {
-      return cb(err, user)
+    let username = profile.username;
+    let email = profile['emails'][0]['value'];
+
+    User.findOrCreate({ username: username, email: email, active: true }, function (err, user) {
+      return cb(err, user);
     });
   }
 ));
 
-
-
-    //select all data from federated credentials where provider = issuer and subject = profileid
-    // db.get('SELECT * FROM federated_credentials WHERE provider = ? AND subject = ?', [
-    //   issuer,
-    //   profile.id
-    // ], function(err, row) {
-    //   if (err) { return cb(err); }
-    //   //if no data is found
-    //   if (!row) {
-    //     //add this user to User table
-    //     db.run('INSERT INTO users (name) VALUES (?)', [
-    //       profile.displayName
-    //     ], function(err) {
-    //       if (err) { return cb(err); }
-
-    //       var id = this.lastID;
-    //       //insert new credentials id, issuer and profile.id
-
-
-
-    //       db.run('INSERT INTO federated_credentials (user_id, provider, subject) VALUES (?, ?, ?)', [
-    //         id,
-    //         issuer,
-    //         profile.id
-    //       ], function(err) {
-    //         if (err) { return cb(err); }
-    //         var user = {
-    //           id: id,
-    //           name: profile.displayName
-    //         };
-    //         return cb(null, user);
-    //       });
-    //     });
-    //     //if the credentials already exists
-    //   } else {
-    //     db.get('SELECT rowid AS id, * FROM users WHERE rowid = ?', [ row.user_id ], function(err, row) {
-    //       if (err) { return cb(err); }
-    //       if (!row) { return cb(null, false); }
-    //       return cb(null, row);
-    //     });
-    //   }
-    // });
-
-
-
-  passport.serializeUser(function(user, cb) {
-    process.nextTick(function() {
-      cb(null, { id: user.id, username: user.username });
-    });
+passport.serializeUser(function (user, cb) {
+  process.nextTick(function () {
+    cb(null, { id: user.id, username: user.username });
   });
+});
 
-  passport.deserializeUser(function(user, cb) {
-    process.nextTick(function() {
-      return cb(null, user);
-    });
+passport.deserializeUser(function (user, cb) {
+  process.nextTick(function () {
+    return cb(null, user);
   });
+});
